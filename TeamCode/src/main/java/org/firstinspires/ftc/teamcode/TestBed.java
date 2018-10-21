708300a7a97cfedec453091923e0b7b457252ff9
Eric Weber <eric.weber.r@gmail.com>
2018-10-20 23:42:16 -0400

    Fixed Testbed post merge

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestBed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestBed.java
@@ -3,9 +3,12 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.rev.RevTouchSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.teamcode.Subsystems.ArmSystem;
+import org.firstinspires.ftc.teamcode.Subsystems.AutomaticClaw;
 import org.firstinspires.ftc.teamcode.Subsystems.ClawSystem;
 import org.firstinspires.ftc.teamcode.Subsystems.MechenumDrive;
 
@@ -15,11 +18,32 @@ public class TestBed extends LinearOpMode {
 
     MechenumDrive Drive = new MechenumDrive();
     ArmSystem Arm = new ArmSystem();
-    ClawSystem Claw = new ClawSystem();
+    AutomaticClaw Claw = new AutomaticClaw();
 
     @Override
     public void runOpMode() throws InterruptedException {
-        final DcMotor init_drive[] ={
+
+        this.Initialize();
+        Claw.OpenClaw();
+
+        waitForStart();
+
+        while (opModeIsActive()){
+            Drive.Drive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+            //telemetry.addLine("Power").addData("FL",Power[0]).addData("FR",Power[1])
+            //        .addData("RL",Power[2]).addData("RR",Power[3]);
+            Arm.ArmLiftPow(gamepad2.left_stick_y);
+            Arm.ExtendArmPow(gamepad2.right_stick_y);
+            if (gamepad2.b) Claw.OpenClaw();
+            if (gamepad2.a) Claw.CloseClaw();
+            telemetry.update();
+            idle();
+        }
+    }
+
+
+    public void Initialize(){
+        final DcMotor init_drive[] = {
                 hardwareMap.dcMotor.get("D_FL"),
                 hardwareMap.dcMotor.get("D_FR"),
                 hardwareMap.dcMotor.get("D_RL"),
@@ -39,37 +63,18 @@ public class TestBed extends LinearOpMode {
         final RevTouchSensor init_Extend_Touch[] = {
                 hardwareMap.get(RevTouchSensor.class, "A_TILT_UP_T"),
                 hardwareMap.get(RevTouchSensor.class, "A_TILT_DOWN_T")};
+        final ColorSensor CS[] = {hardwareMap.get(ColorSensor.class, "CSLeft"),
+                hardwareMap.get(ColorSensor.class, "CSCent"),
+                hardwareMap.get(ColorSensor.class, "CSRight")};
+        final DistanceSensor DS[] = {hardwareMap.get(DistanceSensor.class, "CSLeft"),
+                hardwareMap.get(DistanceSensor.class, "CSCent"),
+                hardwareMap.get(DistanceSensor.class, "CSRight")};
+        final Servo CServo[] = {hardwareMap.servo.get("C_Left"),
+                hardwareMap.servo.get("C_Right")};
 
         Drive.Initialize(init_drive);
-<<<<<<< HEAD
-        Arm.Initialize(init_Arm, init_Extend);
-        //Claw.Initilize(init_Claw);
-=======
         Arm.Initialize(init_Arm, init_Extend, init_Arm_Touch, init_Extend_Touch);
-        Claw.Initilize(init_Claw);
->>>>>>> origin/soren
+        Claw.Initialize(CServo, CS, DS);
 
-        waitForStart();
-
-        while (opModeIsActive()){
-<<<<<<< HEAD
-            double Power[] = Drive.Drive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
-            //telemetry.addLine("Power").addData("FL",Power[0]).addData("FR",Power[1])
-            //        .addData("RL",Power[2]).addData("RR",Power[3]);
-            Arm.ArmLiftPow(gamepad2.left_stick_y);
-            Arm.ExtendArmPow(gamepad2.right_stick_y);
-            //Claw.ClawPos(gamepad2.a);
-=======
-            Arm.ArmLiftPow(gamepad2.left_stick_y);
-            Arm.ExtendArmPow(gamepad2.right_stick_y);
-            Claw.ClawPos(gamepad2.a);
-            telemetry.addData("ESO: ", Arm.CurrentTouchStatus()[0])
-                    .addData(" ESI: ", Arm.CurrentTouchStatus()[1])
-                    .addData(" ASU: ", Arm.CurrentTouchStatus()[2])
-                    .addData(" ESD: ", Arm.CurrentTouchStatus()[3]);
->>>>>>> origin/soren
-            telemetry.update();
-            idle();
-        }
     }
 }
